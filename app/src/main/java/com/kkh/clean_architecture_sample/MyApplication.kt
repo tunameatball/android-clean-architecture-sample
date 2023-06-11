2a30fd2cdc70363dcf673e7b58881058afee41f3
tunameatball <kh9768@gmail.com>
2023-06-11 15:43:39 +0900

    MyApplication 클래스 추가

---
+++ app/src/main/java/com/kkh/clean_architecture_sample/MyApplication.kt
@@ -0,0 +1,10 @@
+package com.kkh.clean_architecture_sample
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication : Application() {
+
+
+}
