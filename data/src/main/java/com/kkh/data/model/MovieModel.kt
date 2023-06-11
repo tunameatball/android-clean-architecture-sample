80fcc010ce36d08749034e5ff6090d217043f634
tunameatball <kh9768@gmail.com>
2023-06-11 14:58:16 +0900

    Movie response Model 생성

---
+++ data/src/main/java/com/kkh/data/model/MovieModel.kt
@@ -0,0 +1,29 @@
+package com.kkh.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class MovieModel(
+    @SerializedName("actor")
+    val actor: String,
+
+    @SerializedName("director")
+    val director: String,
+
+    @SerializedName("image")
+    val image: String,
+
+    @SerializedName("link")
+    val link: String,
+
+    @SerializedName("pubDate")
+    val pubDate: String,
+
+    @SerializedName("subtitle")
+    val subtitle: String,
+
+    @SerializedName("title")
+    val title: String,
+
+    @SerializedName("userRating")
+    val userRating: String
+)
---
+++ data/src/main/java/com/kkh/data/model/MovieResponse.kt
@@ -0,0 +1,21 @@
+package com.kkh.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class MovieResponse(
+    @SerializedName("display")
+    val display: Int,
+
+    @SerializedName("items")
+    val items: List<MovieModel>,
+
+    @SerializedName("lastBuildDate")
+    val lastBuildDate: String,
+
+    @SerializedName("start")
+    val start: Int,
+
+    @SerializedName("total")
+    val total: Int
+
+)
